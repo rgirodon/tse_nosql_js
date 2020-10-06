84bf7361db0b077592b9d2cb4ec5310c515fae50
rgirodon <remy.girodon@gmail.com>
2020-10-06 21:45:29 +0200

    Upgrade neo4j

--- neo4j/index.js
+++ neo4j/index.js
@@ -1,6 +1,6 @@
-var neo4j = require('neo4j-driver').v1;
+var neo4j = require('neo4j-driver');
 
-var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "rgirodon"));
+var driver = neo4j.driver("neo4j://localhost", neo4j.auth.basic("neo4j", "RafaelYanice10"));
 
 var session = driver.session();
 
